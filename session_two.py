818f4ac751020ac76fc77dc420c5bf5e86d24673
ykchoi101 <ppoisoner@gmail.com>
2026-08-19 11:47:30 +0900

    mydonga 11:47 내용 삭제 함수호출

--- session_two.py
+++ session_two.py
@@ -5,5 +5,4 @@ def order():
     if 'user_list'  in st.session_state:
         st.info(f'저장된 총 세션갯수: {len(st.session_state.user_list)}개')
         st.write('세션목록', st.session_state.user_list)
-    else:
-        st.write('세션목록에 없습니다')
+  
