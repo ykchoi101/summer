ad53146a2a79db7340d3b1280f14918e5ed1af27
unknown <ppoisoner@gmail.com>
2026-08-19 12:07:30 +0900

    mydonga update

--- session_first.py
+++ session_first.py
@@ -4,26 +4,7 @@ import random
 import session_two
 
 st.header('st.session_state 세션 테스트')
-def main():
-    if 'user_list' not in st.session_state:
-        st.session_state.user_list = []
 
-
-main() #함수에서 세션기술
-
-@st.cache_data
-def slow_cal(x):
-  
-    time.sleep(1)
-    cal = x*100
-    return cal
-
-dice = [1,2,3,4,5,6]
-pick = random.choice(dice)
-st.write('pick = ',pick)
-result = slow_cal(pick)
-st.write('slow_cal(x)함수 결과 = ' , result)
-# (.venv) C:\~~> streamlit run session_first.py
 
 
 
--- session_two.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def order():
-    st.write('-------------- session_two.py문서  order() --------------')
-    if 'user_list'  in st.session_state:
-        st.info(f'저장된 총 세션갯수: {len(st.session_state.user_list)}개')
-        st.write('세션목록', st.session_state.user_list)
-  
